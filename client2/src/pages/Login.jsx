99a877babce674f24a1e7569f36375a46e2c6557
Shobby789 <smshoaib2001@gmail.com>
2024-02-21 22:28:10 +0500

    initial commit

--- client2/src/pages/Login.jsx
+++ client2/src/pages/Login.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="container-fluid">
-      <h2 className="">Login</h2>
+    <div className="container-fluid auth-page min-vh-100 p-0 d-flex">
+      <div className="left-side bg-dark min-vh-100 w-50"></div>
+      <div className="right-side min-vh-100 w-50"></div>
     </div>
   );
 };
